d7b25a5398029abf5ae9f44a1e9ff8d64572dbe7
hyuk <k941026h@naver.com>
2025-03-17 23:16:25 +0900

    project integration to mono repo

---
+++ platform-service/src/main/java/com/bb/platformservice/PlatformServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bb.platformservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformServiceApplication.class, args);
+	}
+
+}
