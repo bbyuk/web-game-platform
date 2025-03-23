a2bc5c559d7426c64e521fdab4b06e301654618b
hyuk <k941026h@naver.com>
2025-03-24 00:16:21 +0900

    캔버스 컴포넌트 샘플 추가

--- frontend/src/components/canvas/index.js
+++ frontend/src/components/canvas/index.js
@@ -1,39 +1,61 @@
 import "index.css";
 import {createCanvas} from "canvas";
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 
 export default function Canvas() {
     const canvasRef = useRef(null);
     const elementId = "canvas";
+    const [painting, setPainting] = useState(false);
+    const [canvasContext, setCanvasContext] = useState(null);
+    const [currentStroke, setCurrentStroke] = useState([]);
+    const [strokes, setStrokes] = useState([]);
+
+
+    const startPainting = () => {
+        setPainting(true);
+    }
+    const stopPainting = () => {
+        setPainting(false);
+    }
+    const onMouseMove = (event) => {
+        const rect = event.currentTarget.getBoundingClientRect();
+        const offsetX = event.clientX - rect.left;
+        const offsetY = event.clientY - rect.top;
+
+        if (painting) {
+            canvasContext.lineTo(offsetX, offsetY);
+            canvasContext.stroke();
+            setCurrentStroke((prevItems) => [...prevItems, {x: offsetX, y: offsetY}] );
+        }
+        else {
+            canvasContext.beginPath();
+            canvasContext.moveTo(offsetX, offsetY);
+            setStrokes((prevItems) => [...prevItems, currentStroke]);
+        }
+    }
+
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
 
-        // 배경색 설정
-        ctx.fillStyle = '#f0f0f0';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.lineWidth = 10;
+        ctx.lineCap = "round";
+        ctx.strokeStyle = "black";
 
-        // 원 그리기
-        ctx.fillStyle = 'green';
-        ctx.beginPath();
-        ctx.arc(100, 100, 50, 0, Math.PI * 2);
-        ctx.fill();
-
-        // 텍스트 그리기
-        ctx.fillStyle = 'black';
-        ctx.font = '20px Arial';
-        ctx.fillText('Hello, Canvas!', 50, 200);
+        setCanvasContext(ctx);
     }, []);
-
 
     return <>
         <canvas
             id={elementId}
             ref={canvasRef}
-            width={400}
-            height={400}
-            style={{border: '1px solid black'}}
+            width={1000}
+            height={700}
+            onMouseMove={e => onMouseMove(e)}
+            onMouseDown={startPainting}
+            onMouseUp={stopPainting}
+            onMouseLeave={stopPainting}
         />
     </>;
 }
