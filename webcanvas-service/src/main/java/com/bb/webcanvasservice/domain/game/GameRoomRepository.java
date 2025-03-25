31a6afe6293124246f4bb008b41261e3edfae0af
hyuk <k941026h@naver.com>
2025-03-26 00:35:06 +0900

    GameRoomRepository 유저토큰으로 유효 방 찾기 쿼리 / joinCode 체크용 쿼리 추가 및 비관적 락 적용

--- webcanvas-service/src/main/java/com/bb/webcanvasservice/domain/game/GameRoomRepository.java
+++ webcanvas-service/src/main/java/com/bb/webcanvasservice/domain/game/GameRoomRepository.java
@@ -1,6 +1,10 @@
 package com.bb.webcanvasservice.domain.game;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -8,4 +12,24 @@ import java.util.Optional;
 @Repository
 public interface GameRoomRepository extends JpaRepository<GameRoom, Long> {
 
+
+    @Query("""
+            select      gre.gameRoom
+            from        GameRoomEntrance gre
+            join fetch GameRoom gr
+            on          gre.gameRoom = gr
+            where       gr.status != :gameRoomStatus
+            and         gre.user.userToken = :userToken
+            """)
+    Optional<GameRoom> findByGameRoomStatusNotMatchedAndUserToken(@Param("gameRoomStatus") GameRoomStatus gameRoomStatus, @Param("userToken") String userToken);
+
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("""
+            select  gr
+            from    GameRoom gr
+            where   gr.status != :gameRoomStatus
+            and     gr.joinCode = :joinCode
+            """)
+    Optional<GameRoom> findByGameRoomStatusNotMatchedAndJoinCode(@Param("gameRoomStatus") GameRoomStatus gameRoomStatus, @Param("joinCode") String joinCode);
 }
