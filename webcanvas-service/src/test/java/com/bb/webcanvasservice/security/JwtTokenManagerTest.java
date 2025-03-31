7c3c52239933bf3ac0e099e67577e48994777927
hyuk <hyuk.kang1@cj.net>
2025-03-31 14:00:07 +0900

    JwtTokenManagetTest 테스트 코드 추가

---
+++ webcanvas-service/src/test/java/com/bb/webcanvasservice/security/JwtTokenManagerTest.java
@@ -0,0 +1,25 @@
+package com.bb.webcanvasservice.security;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class JwtTokenManagerTest {
+
+    private JwtManager jwtManager = new JwtManager();
+
+    @Test
+    @DisplayName("userId로 토큰 만들기 - 토큰으로부터 userId 파싱")
+    void generateTokenAndGetUserIdFromToken() {
+        // given
+        Long userId = 1L;
+
+        // when
+        String token = jwtManager.generateToken(userId);
+
+        // then
+        Assertions.assertThat(token).isNotBlank();
+        Assertions.assertThat(jwtManager.getUserIdFromToken(token)).isEqualTo(userId);
+    }
+
+}
