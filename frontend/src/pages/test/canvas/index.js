f93afcbbea92f2b84e237bea9184785c79305ed0
hyuk <k941026h@naver.com>
2025-04-01 21:22:46 +0900

    canvas 테스트 page 버튼 추가

--- frontend/src/pages/test/canvas/index.js
+++ frontend/src/pages/test/canvas/index.js
@@ -27,6 +27,8 @@ export default function CanvasTest() {
         <div>
             <br/>
             <br/>
+            <button onClick={e => alert("테스트 유저 만들기")}>테스트 유저 만들기</button>
+            <button onClick={e => alert("테스트 방 만들기")}>테스트 방 만들기</button>
             <button onClick={e => console.log(strokes)}>현재까지의 데이터 로그 찍기</button>
             <button onClick={clear}>전체 지우기</button>
             <br/>
