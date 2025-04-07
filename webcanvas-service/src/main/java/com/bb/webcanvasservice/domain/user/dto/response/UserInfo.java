642faeb6a4ffa37c704812454161e3b441e6f183
bbyuk <k941026h@naver.com>
2025-04-07 12:37:54 +0900

    UserInfo API Doc Schema 주석 추가

--- webcanvas-service/src/main/java/com/bb/webcanvasservice/domain/user/dto/response/UserInfo.java
+++ webcanvas-service/src/main/java/com/bb/webcanvasservice/domain/user/dto/response/UserInfo.java
@@ -1,11 +1,16 @@
 package com.bb.webcanvasservice.domain.user.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /**
- * 유저 정보 리턴 dto
+ * 유저 정보 응답 DTO
  * @param userId 유저 식별 ID
- * @param userToken
+ * @param fingerprint
  */
+@Schema(description = "유저 정보 응답 DTO")
 public record UserInfo(
+        @Schema(description = "유저 식별 ID", example = "15")
         Long userId,
-        String userToken
+        @Schema(description = "클라이언트 Fingerprint")
+        String fingerprint
 ) {}
