595c33efde49e648437eccdcaf22f3b80e3766ba
bbyuk <k941026h@naver.com>
2025-04-09 12:38:35 +0900

    캔버스 테스트 페이지 테스트메소드 추가

--- frontend/src/pages/test/canvas/index.js
+++ frontend/src/pages/test/canvas/index.js
@@ -17,12 +17,39 @@ export default function CanvasTest() {
     };
 
     const createGameRoom = () => {
-        fetch(`${apiBaseUrl}/users`)
-            .then(response => response.json())
-            .then(data => console.log(data));
+
     };
 
-    return <div >
+    const createUser = async (e) => {
+        try {
+            const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/user`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    clientFingerprint: "testuser1234"
+                })
+            });
+
+            if (!response.ok) {
+                const msg = "API 요청 실패 " + response.status;
+                alert(msg);
+                throw new Error(msg);
+            }
+
+            const {userId, fingerprint} = await response.json();
+
+            localStorage.setItem("fingerprint", fingerprint);
+            localStorage.setItem("userId", userId);
+
+        } catch (error) {
+            alert("API 요청 실패 " + error);
+            console.log(error);
+        }
+    };
+
+    return <div>
         <Canvas
             strokes={strokes}
             onStroke={onStrokeHandler}
@@ -33,7 +60,7 @@ export default function CanvasTest() {
         <div>
             <br/>
             <br/>
-            <button onClick={e => alert("테스트 유저 만들기")}>테스트 유저 만들기</button>
+            <button onClick={createUser}>테스트 유저 만들기</button>
             <button onClick={e => alert("테스트 방 만들기")}>테스트 방 만들기</button>
             <button onClick={e => console.log(strokes)}>현재까지의 데이터 로그 찍기</button>
             <button onClick={clear}>전체 지우기</button>
