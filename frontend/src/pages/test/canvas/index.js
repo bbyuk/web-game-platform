6e6b9be257c436db74213b8b18be2009542724d9
bbyuk <k941026h@naver.com>
2025-04-09 16:55:20 +0900

    캔버스 테스트 페이지 중간 커밋

--- frontend/src/pages/test/canvas/index.js
+++ frontend/src/pages/test/canvas/index.js
@@ -4,6 +4,7 @@ import {useState} from "react";
 export default function CanvasTest() {
     const [strokes, setStrokes] = useState([]);
     const [reRenderingSignal, setReRenderingSignal] = useState(false);
+    const [userCreating, setUserCreating] = useState(false);
 
     const onStrokeHandler = (stroke) => {
         if (stroke.length > 0) {
@@ -20,8 +21,20 @@ export default function CanvasTest() {
 
     };
 
-    const createUser = async (e) => {
+    const onCreateUserButtonClickHandler = async (e) => {
+        const created = await createUser();
+
+        if (created) {
+            alert(`${localStorage.getItem("userId")} user created!`);
+        }
+    }
+    const createUser = async () => {
+        if (userCreating) {
+            alert("이미 요청을 처리중입니다");
+            return false;
+        }
         try {
+            setUserCreating(true);
             const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/user`, {
                 method: "POST",
                 headers: {
@@ -33,20 +46,23 @@ export default function CanvasTest() {
             });
 
             if (!response.ok) {
-                const msg = "API 요청 실패 " + response.status;
-                alert(msg);
-                throw new Error(msg);
+                throw new Error(response.status);
             }
 
             const {userId, fingerprint} = await response.json();
 
             localStorage.setItem("fingerprint", fingerprint);
             localStorage.setItem("userId", userId);
-
         } catch (error) {
             alert("API 요청 실패 " + error);
             console.log(error);
+            return false;
         }
+        finally {
+            setUserCreating(false);
+        }
+
+        return true;
     };
 
     return <div>
@@ -60,7 +76,7 @@ export default function CanvasTest() {
         <div>
             <br/>
             <br/>
-            <button onClick={createUser}>테스트 유저 만들기</button>
+            <button onClick={onCreateUserButtonClickHandler}>테스트 유저 만들기</button>
             <button onClick={e => alert("테스트 방 만들기")}>테스트 방 만들기</button>
             <button onClick={e => console.log(strokes)}>현재까지의 데이터 로그 찍기</button>
             <button onClick={clear}>전체 지우기</button>
