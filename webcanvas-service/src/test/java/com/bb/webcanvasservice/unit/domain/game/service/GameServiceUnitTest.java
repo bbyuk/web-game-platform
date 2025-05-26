8684a05e19dff924f69bc7100be51badac70d65f
bbyuk <k941026h@naver.com>
2025-05-26 17:27:32 +0900

    [webcanvas][back] 다음 턴 그림 그릴 사람 조회 밸런스 처리 테스트 코드 추가

--- webcanvas-service/src/test/java/com/bb/webcanvasservice/unit/domain/game/service/GameServiceUnitTest.java
+++ webcanvas-service/src/test/java/com/bb/webcanvasservice/unit/domain/game/service/GameServiceUnitTest.java
@@ -166,11 +166,54 @@ class GameServiceUnitTest {
         // then
     }
 
+    @Test
+    @DisplayName("다음 턴 그림 그릴 사람 조회 - 그림 그린 턴 집계해 골고루 분배 되도록 처리")
+    void findNextDrawerBalancedCheck() throws Exception {
+        // given
+        User user1 = userRepository.save(new User(FingerprintGenerator.generate()));
+        User user2 = userRepository.save(new User(FingerprintGenerator.generate()));
+        User user3 = userRepository.save(new User(FingerprintGenerator.generate()));
+
+        GameRoom gameRoom = gameRoomRepository.save(new GameRoom(JoinCodeGenerator.generate(6)));
+
+        GameRoomEntrance gameRoomEntrance1 = gameRoomEntranceRepository.save(new GameRoomEntrance(gameRoom, user1, "유저1", GameRoomRole.HOST));
+        gameRoomEntrance1.changeReady(true);
+
+        GameRoomEntrance gameRoomEntrance2 = gameRoomEntranceRepository.save(new GameRoomEntrance(gameRoom, user2, "유저2", GameRoomRole.GUEST));
+        gameRoomEntrance2.changeReady(true);
+
+        GameRoomEntrance gameRoomEntrance3 = gameRoomEntranceRepository.save(new GameRoomEntrance(gameRoom, user3, "유저3", GameRoomRole.GUEST));
+        gameRoomEntrance3.changeReady(true);
+
+        List<GameRoomEntrance> entrances = List.of(gameRoomEntrance1, gameRoomEntrance2, gameRoomEntrance3);
+
+        GameStartRequest gameStartRequest = new GameStartRequest(gameRoom.getId(), 5, 90);
+        Long gameSessionId = gameService.startGame(gameStartRequest, user1.getId());
+
+        GameSession gameSession = gameSessionRepository.findById(gameSessionId).get();
+
+        /**
+         * 현재 게임세션의 제한 턴을 모두 사용
+         */
+        gameTurnRepository.save(new GameTurn(gameSession, user1, "랜덤 명사"));
+        gameTurnRepository.save(new GameTurn(gameSession, user2, "랜덤 명사2"));
+        gameTurnRepository.save(new GameTurn(gameSession, user3, "핸덤 명사3"));
+        gameTurnRepository.save(new GameTurn(gameSession, user3, "핸덤 명사123"));
+
+
+        // when
+
+        /**
+         * 3L -> 2회
+         * 1L, 2L -> 1회
+         */
+        Long nextDrawer = gameService.findNextDrawer(gameSessionId);
+        Assertions.assertThat(List.of(1L, 2L)).contains(nextDrawer);
+    }
 
     @Test
-    @DisplayName("다음 턴 그림그릴 사람 조회 - 턴 리밋을 넘으면 실패")
+    @DisplayName("다음 턴 그림 그릴 사람 조회 - 턴 리밋을 넘으면 실패")
     void findNextDrawerFailedWhenTurnLimitOver() throws Exception {
-        // given
         // given
         User user1 = userRepository.save(new User(FingerprintGenerator.generate()));
         User user2 = userRepository.save(new User(FingerprintGenerator.generate()));
@@ -200,9 +243,6 @@ class GameServiceUnitTest {
         gameTurnRepository.save(new GameTurn(gameSession, user1, "랜덤 명사"));
         gameTurnRepository.save(new GameTurn(gameSession, user2, "랜덤 명사2"));
         gameTurnRepository.save(new GameTurn(gameSession, user3, "핸덤 명사3"));
-
-        em.flush();
-        em.close();
 
         // when
         Assertions.assertThatThrownBy(() -> gameService.findNextDrawer(gameSessionId))
